269c422267cb04528b9f47f70c16f45eb0c6edd5
MARIE_M <fifi.maresova@gmail.com>
2020-11-28 10:37:48 +0100

    push :)

---
+++ src/footer/footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = (props) => {
+  return (
+    <div class="footer">
+      <ul class="footer_list">
+        <li class="footer_item">
+          <a href="index.html">Homepage</a>
+        </li>
+        <li class="footer_item">
+          <a href="swapRules.html">Pravidla</a>
+        </li>
+        <li class="footer_item">
+          <a href="">položka patičky</a>
+        </li>
+        <li class="footer_item">
+          <a href="">položka patičky</a>
+        </li>
+        <li class="footer_item">
+          <a href="">položka patičky</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Footer;
--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,17 @@ import { render } from 'react-dom';
 import './index.html';
 import '../src/styles.css';
 import '../src/hamburger.css';
+import '../src/pages/footer.css';
 import MainCategory from '../src/category/category.jsx';
+import Footer from '../src/pages/footer.jsx';
 
 const App = () => {
-  return <MainCategory />;
+  return (
+    <>
+      <MainCategory />
+      <Footer />
+    </>
+  );
 };
 
 render(<App />, document.querySelector('#app'));
