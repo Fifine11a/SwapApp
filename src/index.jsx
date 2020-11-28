d43055678e37e93b6249ade193d13e168d4de80e
Magdalena H. Selingerová <selingerova.magdalena@gmail.com>
2020-11-28 10:34:23 +0100

    zapojeni routeru

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
+import App from './app';
 import './index.html';
 import '../src/styles.css';
 import '../src/hamburger.css';
-import MainCategory from '../src/category/category.jsx';
-
-const App = () => {
-  return <MainCategory />;
-};
 
 render(<App />, document.querySelector('#app'));
