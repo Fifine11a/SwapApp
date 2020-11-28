a6b13f69d6cd91ac2084465647bc215390874834
Magdalena H. Selingerová <selingerova.magdalena@gmail.com>
2020-11-28 20:54:44 +0100

    listování kategorií

--- src/pages/categoryPage/categoryPage.jsx
+++ src/pages/categoryPage/categoryPage.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './categoryPage.css';
-import categories from '../../data/category.json';
+import db from '../../firestore.js';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Search from '../../search/search.jsx';
 
 const MainCategory = (props) => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    db.collection('categories')
+      .get()
+      .then((result) =>
+        result.docs.map((e) => {
+          const data = e.data();
+          return {
+            ...data,
+            id: e.id,
+          };
+        }),
+      )
+      .then((data) => setCategories(data));
+  }, []);
   return (
     <div className="demandPage mediaQueries">
       <div className="header">
@@ -14,6 +30,7 @@ const MainCategory = (props) => {
       <div className="categoryBtn">
         {categories.map((category) => (
           <Link
+            key={category.id}
             className="category searchCategoryClothes"
             to={`/kategorie/${category.id}`}
           >
