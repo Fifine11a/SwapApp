29f20abfd84f26af6f426c24bb0f395653d6e6f1
Magdalena H. Selingerová <selingerova.magdalena@gmail.com>
2020-11-28 11:06:04 +0100

    homepage2

--- src/app.jsx
+++ src/app.jsx
@@ -8,20 +8,6 @@ export default function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home page</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
@@ -43,12 +29,4 @@ export default function App() {
       </>
     </Router>
   );
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
 }
