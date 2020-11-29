5ac0df516ac6216d25679bddf0c1c67ff4fb595c
MARIE_M <fifi.maresova@gmail.com>
2020-11-29 17:49:46 +0100

    logo

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import Button from '../button/button';
+import Search from '../search/search';
+import './header.css';
+import logo from '../img/logoHeader.png';
 
 const Header = () => {
   return (
-    <>
+    <div className="header">
       <div className="logo">
-        <img className="logo_male" src="img/logo.png" alt="swapApp logo" />
+        <img className="logo_male" src={logo} alt="swapApp logo" />
       </div>
+      <Search />
       <Button title="Nabízím" location="'/formular/novy'" />
-    </>
+    </div>
   );
 };
 
