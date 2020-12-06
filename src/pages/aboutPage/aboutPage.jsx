9964cfbbeaac0bb5a0b0d4e933d83a99556e307f
Magdalena H. Selingerová <selingerova.magdalena@gmail.com>
2020-12-06 16:39:33 +0100

    about us page - pokus scroll to

--- src/pages/aboutPage/aboutPage.jsx
+++ src/pages/aboutPage/aboutPage.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './aboutPage.css';
 import czechitas from '../../img/Czechitas.png';
 import pipedrive from '../../img/pipedrive.png';
 
 const AboutUs = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  });
   return (
     <div className="aboutUsPage">
       <h1>SwapApp by Marie&Magdalena</h1>
