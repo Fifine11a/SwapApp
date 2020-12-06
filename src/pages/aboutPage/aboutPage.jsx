003fa375557c8b7ca76aeb67a0a0625c416378a9
Magdalena H. Selingerová <selingerova.magdalena@gmail.com>
2020-12-06 14:16:05 +0100

    about us page a loga

--- src/pages/aboutPage/aboutPage.jsx
+++ src/pages/aboutPage/aboutPage.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './aboutPage.css';
+import czechitas from '../../img/Czechitas.png';
+import pipedrive from '../../img/pipedrive.png';
 
 const AboutUs = () => {
   return (
@@ -10,10 +13,11 @@ const AboutUs = () => {
         Digitální akademie:Web 2020.
       </p>
       <p>
-        Děkujeme{' '}
+        Děkujeme Czechitas
+        {/* {' '}
         <a href="https://www.czechitas.cz/cs/">
-          <img src="../../img/Czechitas.png" alt="logo Czechitas" />
-        </a>{' '}
+          <img src={czechitas} alt="logo Czechitas" />
+        </a>{' '} */}
         za organizaci, Martinovi, Filipům, Sergejovi, Petrovi a spol za
         lektorování a koučování.
       </p>
@@ -25,10 +29,28 @@ const AboutUs = () => {
         Náš nekonečný vděk patří taky Lukášovi, Rosťovi a Tomášovi z Pipedrive
         za mentorování a láskyplnou péči. A fotbálkové bitvy. A víno.
       </p>
-      <div className="mailBtnElm">
-        <a href={`mailto: swapapp@zohomail.eu`}>
-          <button className="mailBtn">Napište nám</button>
-        </a>
+      <div className="logo_contact">
+        <div className="logoElm">
+          <Link className="czechitas" to="https://www.czechitas.cz/cs/">
+            <img
+              className="czechitasLogo"
+              src={czechitas}
+              alt="logo czechitas"
+            />{' '}
+          </Link>
+          <Link className="pipedrive" to="https://www.pipedrive.com/">
+            <img
+              className="pipedriveLogo"
+              src={pipedrive}
+              alt="logo pipedrive"
+            />{' '}
+          </Link>
+        </div>
+        <div>
+          <a href={`mailto: swapapp@zohomail.eu`}>
+            <button className="mailBtn">Napište nám</button>
+          </a>
+        </div>
       </div>
     </div>
   );
